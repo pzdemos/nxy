fb09f8d2dafd450a4798fb99c08b1b74df1c9222
彭振辉 <94799799+pzdemos@users.noreply.github.com>
2023-10-26 02:08:01 +0800

    Delete Desktop directory

--- Desktop/springhyg02/pzshop_types.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO pzshop.types (id, name) VALUES (1, '家居生活');
-INSERT INTO pzshop.types (id, name) VALUES (2, '文体周边');
-INSERT INTO pzshop.types (id, name) VALUES (3, '秋冬新款');
--- Desktop/springhyg02/pzshop_users.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO pzshop.users (id, username, password, email, phone, isadmin) VALUES (1, '小雪', '123456', '123@1qq.com', '18770412740', '1');
-INSERT INTO pzshop.users (id, username, password, email, phone, isadmin) VALUES (2, '小张', '123456', '456@qq.com', '17687968419', null);
-INSERT INTO pzshop.users (id, username, password, email, phone, isadmin) VALUES (3, 'z', '1', '111@qq.com', '183271', '1');
